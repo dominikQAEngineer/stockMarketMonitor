0ab934d88c59afbd94e5ae7f5d3edf76fb8c8c0e
Dominik Platos <dominik.platos@gmail.com>
2017-03-22 23:53:45 +0100

    sms

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -92,10 +92,10 @@ var app = {
         //CONFIGURATION
         var options = {
             replaceLineBreaks: false, // true to replace \n by a new line, false by default
-            android: {
-                intent: 'INTENT'  // send SMS with the native android SMS messaging
-                //intent: '' // send SMS without open any other app
-            }
+//            android: {
+//                intent: 'INTENT'  // send SMS with the native android SMS messaging
+//                //intent: '' // send SMS without open any other app
+//            }
         };
 
         var success = function () { alert('Message sent successfully'); };
