39bdfe0805fc0111d4f7d73f085bb2be66810e90
Dominik Platos <dominik.platos@gmail.com>
2017-05-18 00:34:31 +0200

    Pierwsze pobranie html innej strony! Widoczne tylko w logu (F12 kliknąć na przegladarce)

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -34,8 +34,23 @@ function personalInfo() {
 	navigator.notification.alert(info);
 
 }
-function checkConnection() {
+function makeHttpObject() {
+  $(document).ready(function() {
+              var symbol='goog';
+              var callback = function(data) {
+                      var price=data.query.results.span[0].content;
+                      alert('Stock Price: ' + price);
+                  };
 
+              var url = 'http://query.yahooapis.com/v1/public/yql';
+              // this is the lovely YQL query (html encoded) which lets us get the stock price:
+              // select * from html where url="http://finance.yahoo.com/q?s=goog" and xpath='//span[@id="yfs_l10_goog"]'
+              var data = "q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Ffinance.yahoo.com%2Fq%3Fs%3D" + symbol + "%22%20and%20xpath%3D'%2F%2Fspan%5B%40id%3D%22yfs_l10_" + symbol + "%22%5D'&format=json";
+              $.getJSON(url, data, callback);
+      });
+
+}
+function checkConnection() {
 	var networkState = navigator.connection.type;
 
         var states = {};
@@ -48,7 +63,7 @@ function checkConnection() {
         states[Connection.CELL]     = 'Cell generic connection';
         states[Connection.NONE]     = 'No network connection';
 
-        alert('Connection type: ' + states[networkState]);
+        alert(data);
 
 }
 function onSuccess(acceleration) {
